a08d243f367701cace84f91c127df15f08f7da53
AlexisChen <51479421+AlexisChen99@users.noreply.github.com>
2020-02-03 14:10:28 -0800

    Add files via upload

---
+++ MovieCell.h
@@ -0,0 +1,17 @@
+//
+//  MovieCell.h
+//  Flixy
+//
+//  Created by Alexis Chen on 2/3/20.
+//  Copyright © 2020 Alexis Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MovieCell : UICollectionViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MovieCell.m
@@ -0,0 +1,13 @@
+//
+//  MovieCell.m
+//  Flixy
+//
+//  Created by Alexis Chen on 2/3/20.
+//  Copyright © 2020 Alexis Chen. All rights reserved.
+//
+
+#import "MovieCell.h"
+
+@implementation MovieCell
+
+@end
